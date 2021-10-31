3f96ef9d62289d88359e902e28e84708a9fa149f
Mike Search <search_michael@yahoo.com>
2021-10-31 05:51:15 +0000

    clears git cache, removes client git

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
 const express = require('express');
-
-
-
+const mongoose = require('mongoose');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("DB connected successfully"))
+.catch((err) => console.log(err));
 
 app.get('/', (req, res) => res.send('Server Running'));
 

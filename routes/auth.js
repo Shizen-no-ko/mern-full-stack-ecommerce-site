783f7ccb5262bf3efe6da0059a699cfd4477fad2
Mike Search <search_michael@yahoo.com>
2021-11-01 07:12:24 +0000

    adds body validation to register route

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,21 @@
-const { body } = require('express-validator');
 const router = require('express').Router();
-
+const { body, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
-router.post('/register', (req, res) => {
+router.post('/register', body('email').isEmail(), body('password').isLength({ min: 6 }), body('username').isLength({ min: 1 }),  (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
     const newUser = new User(
         {
             username: req.body.username,
             email: req.body.email,
             password: req.body.password
-    }
+        }
     );
     console.log(newUser);
     // const {password, ...restOfBody} = req.body;
@@ -18,7 +23,7 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-    const {password, ...restOfBody} = req.body;
+    const { password, ...restOfBody } = req.body;
     res.send(restOfBody);
 });
 

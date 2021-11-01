6f2843c6cc59e9f2a9eaabbb0709563d8779fd88
Mike Search <search_michael@yahoo.com>
2021-11-01 07:03:36 +0000

    adds creation of new user to register route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,20 @@
 const { body } = require('express-validator');
-
 const router = require('express').Router();
 
+
+const User = require('../models/User');
+
 router.post('/register', (req, res) => {
-    const {password, ...restOfBody} = req.body;
-    res.send(restOfBody);
+    const newUser = new User(
+        {
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+    }
+    );
+    console.log(newUser);
+    // const {password, ...restOfBody} = req.body;
+    res.send("data received");
 });
 
 router.post('/login', (req, res) => {

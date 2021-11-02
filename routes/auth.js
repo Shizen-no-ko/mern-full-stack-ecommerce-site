1e0a786bc9db466162b926e4e59a9518404d788c
Mike Search <search_michael@yahoo.com>
2021-11-02 10:00:31 +0000

    basic implementation of jwt webtoken in register route

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
+// require('dotenv').config();
 const router = require('express').Router();
 const { body, validationResult } = require('express-validator');
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 
@@ -11,28 +13,43 @@ router.post('/register', body('email').isEmail(), body('password').isLength({ mi
         return res.status(400).json({ errors: errors.array() });
     }
 
-    try{
-        let user = await User.findOne({email: req.body.email});
-        if(user){
-            return res.status(400).json({ errors: [{msg: "User already exists"}]});
+    try {
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ errors: [{ msg: "User already exists" }] });
         }
-    } catch { (err) => console.log(err)
-
+        const encryptedPassword = CryptoJS.AES.encrypt(req.body.password, process.env.ENCRYPTION_SECRET);
+        const newUser = new User(
+            {
+                username: req.body.username,
+                email: req.body.email,
+                password: encryptedPassword
+            }
+        );
+            await newUser.save((err, user) => {
+                if(err) console.log(err);
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+    
+            jwt.sign(
+                payload,
+                process.env.JWT_SECRET,
+                { expiresIn: 3600 },
+                (err, token) => {
+                    if (err) console.log(err);
+                    res.json({ token });
+                });
+            });
+        }
+     catch {
+        (err) => {
+            console.log(err)
+            res.status(500).json("Server Error");
+        }
     }
-
-    const encryptedPassword = CryptoJS.AES.encrypt(req.body.password, process.env.ENCRYPTION_SECRET);
-    const newUser = new User(
-        {
-            username: req.body.username,
-            email: req.body.email,
-            password: encryptedPassword
-        }
-    );
-    // console.log(newUser);
-    const saved = await newUser.save();
-    console.log(`${saved} is saved in DB`);
-    // const {password, ...restOfBody} = req.body;
-    res.send("data received");
 });
 
 router.post('/login', body('email').isEmail(), async (req, res) => {
@@ -43,29 +60,21 @@ router.post('/login', body('email').isEmail(), async (req, res) => {
     try {
         await User.findOne({ email: req.body.email }, function (err, user) {
             if (!user) {
-                res.status(401).json("User not found");
+                return res.status(401).json("User not found");
             }
             const decryptedPassword = CryptoJS.AES.decrypt(user.password, process.env.ENCRYPTION_SECRET).toString(CryptoJS.enc.Utf8);
             console.log(decryptedPassword);
             console.log(decryptedPassword === req.body.password);
             if (decryptedPassword !== req.body.password) {
-                res.status(401).json("Incorrect password");
+                return res.status(401).json("Incorrect password");
             }
             const { password, ...userWithoutPassword } = user._doc;
-            res.status(200).json(userWithoutPassword);
+            return res.status(200).json(userWithoutPassword);
         });
     }
     catch {
         (err) => console.log(err)
     }
-
-
-
-
-    // const { password, ...restOfBody } = req.body;
-    // res.send(restOfBody);
-    // var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
-
 });
 
 

0beddb256f251686bd06fe1a2f7310a3089c288d
Mike Search <search_michael@yahoo.com>
2021-11-04 07:47:43 +0000

    fills out user put route

--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,40 @@
 const router = require('express').Router();
+const CryptoJS = require('crypto-js');
 
 const tokenAuth = require('../middleware/tokenAuth');
 const checkAuthorizedToEdit = require('../middleware/checkAuthorizedToEdit');
+
+const User = require('../models/User');
 
 
 router.get('/test', (req, res) => {
     res.send("Test Works");
 });
 
-router.put('/:id', tokenAuth, checkAuthorizedToEdit, (req, res) => {
-    res.send('Middleware Working');
+router.put('/:id', tokenAuth, checkAuthorizedToEdit,  async (req, res) => {
+    if(req.body.password){
+        encryptedPassword = CryptoJS.AES.encrypt(req.body.password, process.env.ENCRYPTION_SECRET).toString();
+    }
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            username: req.body.username,
+            email: req.body.email,
+            password: encryptedPassword,
+            isAdministrator: req.body.isAdministrator
+        }, {new: true});
+        console.log("username is");
+        console.log(req.body.username);
+        const { password, ...userWithoutPassword } = updatedUser._doc;
+        return res.status(200).json(userWithoutPassword);
+    } 
+    catch(err) {
+        return res.status(500).json({ errors: [{ msg: "Server Error" }] });
+    }
 });
 
 router.patch('/:id', tokenAuth, checkAuthorizedToEdit, (req, res) => {
-    res.send('Middleware Working');
+    res.sedn('Middleware Working');
 });
 
 router.delete('/:id', tokenAuth, checkAuthorizedToEdit, (req, res) => {

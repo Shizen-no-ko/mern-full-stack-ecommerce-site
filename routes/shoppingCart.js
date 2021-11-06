5f9d9baac595ffd96c2c42b2df4cd497ad0aa5e4
Mike Search <search_michael@yahoo.com>
2021-11-06 10:18:39 +0000

    starts shopping cart routes

---
+++ routes/shoppingCart.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+
+const tokenAuth = require('../middleware/tokenAuth');
+const checkAdmin = require('../middleware/checkAdmin');
+const checkAuthorizedToEdit = require('../middleware/checkAuthorizedToEdit');
+
+const ShoppingCart = require('../models/ShoppingCart');
+
+router.post('/add', tokenAuth, async (req, res) => {
+    try {
+        const newCart = new ShoppingCart(req.body);
+            await newCart.save((err, cart) => {
+                if(err) return res.status(500).json({ errors: [{ msg: "Create Cart Error" }] });
+                return res.status(200).json(cart);
+        })
+    }
+     catch {
+        (err) => {
+            console.log(err)
+            res.status(500).json({ errors: [{ msg: "Server Error" }] });
+        }
+    }
+});
+
+
+router.put('/:id', tokenAuth, checkAuthorizedToEdit, async (req, res) => {
+    try {
+        const updatedCart = await ShoppingCart.findByIdAndUpdate(req.params.id, {
+          $set:req.body
+        }, { new: true });
+        return res.status(200).json(updatedCart);
+    }
+    catch (err) {
+        return res.status(500).json({ errors: [{ msg: "Server Error" }] });
+    }
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
+const shoppingCartRoute = require('./routes/shoppingCart');
 
 const PORT = process.env.PORT || 5000;
 
@@ -21,6 +22,7 @@ app.use(express.urlencoded());
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/products', productRoute);
+app.use('/api/carts', shoppingCartRoute);
 
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`);

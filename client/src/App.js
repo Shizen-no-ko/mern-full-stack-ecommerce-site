15f6b48775d2d6e982ee22d20d5013825115e673
Mike Search <search_michael@yahoo.com>
2021-11-29 07:37:15 +0000

    implements basic router

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
 import React, {Fragment} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Landing from './components/pages/Landing';
-// import ProductsFilter from './components/pages/ProductsFilter';
-// import IndividualProduct from './components/pages/IndividualProduct';
-// import Login from './components/pages/Login';
-// import Register from './components/pages/Register';
-// import ShoppingCart from './components/pages/ShoppingCart';
+import ProductsFilter from './components/pages/ProductsFilter';
+import IndividualProduct from './components/pages/IndividualProduct';
+import Login from './components/pages/Login';
+import Register from './components/pages/Register';
+import ShoppingCart from './components/pages/ShoppingCart';
 
 function App() {
   return (
-    <Fragment>
-    <Landing/>
+    <Router>
+ <Fragment>
+   <Route exact path="/" component={Landing} />
+   <Switch>
+   <Route exact path="/login" component={Login} />
+   <Route exact path="/register" component={Register} />
+   <Route exact path="/cart" component={ShoppingCart} />
+   <Route exact path="/product" component={IndividualProduct} />
+   <Route exact path="/products" component={ProductsFilter} />
+   </Switch>
     </Fragment>
+    </Router>
+   
   );
 }
 

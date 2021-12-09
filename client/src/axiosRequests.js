8a358372bfc2635979317a68df0af83496c8d91a
Mike Search <search_michael@yahoo.com>
2021-12-09 07:44:13 +0000

    extends axiosRequests for userRequests

--- client/src/axiosRequests.js
+++ client/src/axiosRequests.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:5000/api';
-console.log("BASE IS :")
-console.log(BASE_URL)
+const TOKEN = '';
 
 
 export const publicReq = axios.create({
@@ -11,5 +10,5 @@ export const publicReq = axios.create({
 
 export const userReq = axios.create({
     baseURL: BASE_URL,
-    header: {token: 'Bearer Token'} 
+    header: { token: `Bearer ${TOKEN}` } 
 })

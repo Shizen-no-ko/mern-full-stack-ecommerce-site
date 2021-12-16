6c5b0485bcb07fc3fe7f55d0877244391b486725
Mike Search <search_michael@yahoo.com>
2021-12-16 06:50:18 +0000

    gets basic admin page set up and running parallel using concurrently

---
+++ admin/src/App.js
@@ -0,0 +1,22 @@
+import React, {Fragment} from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import './App.css';
+
+// import ScrollToTop from './utilities/ScrollToTop';
+
+
+function App() {
+  // const user = useSelector(state => state.user.currentUser);
+  
+
+  return (
+ <Fragment>
+ <h1>ADMIN PAGE</h1>
+    </Fragment>
+  
+   
+  );
+}
+
+export default App;
---
+++ admin/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import { store, persistor }  from './redux/store';
+
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
+

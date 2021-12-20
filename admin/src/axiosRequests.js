bba1f059e6e8c96385e1ba9fa8ffe08e301a8d81
Mike Search <search_michael@yahoo.com>
2021-12-20 07:50:14 +0000

    fixes issue with undefined user token at start up

--- admin/src/axiosRequests.js
+++ admin/src/axiosRequests.js
@@ -3,7 +3,12 @@ import axios from 'axios';
 
 
 const BASE_URL = 'http://localhost:5000/api';
+// const CURRENT_USER = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser;
+// const TOKEN =  CURRENT_USER ? CURRENT_USER.token : null;
 const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.token;
+
+console.log('TOKEN IS')
+console.log(TOKEN);
 
 
 export const publicReq = axios.create({
--- admin/src/redux/userRedux.js
+++ admin/src/redux/userRedux.js
@@ -3,7 +3,10 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice({
     name: 'user',
     initialState: {
-        currentUser: null,
+        currentUser: {
+            token: null,
+            isAdmininstrator: false
+        },
         isFetching: false,
 
     },

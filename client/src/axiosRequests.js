83ec4ce37a18dff80380d147db6835d4a542406f
Mike Search <search_michael@yahoo.com>
2022-02-05 08:04:29 +0100

    fixes headers typo

--- client/src/axiosRequests.js
+++ client/src/axiosRequests.js
@@ -7,6 +7,9 @@ const BASE_URL = 'http://localhost:5000/api';
 const CURRENT_USER = localStorage.length > 0 ? JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser : null;
 const TOKEN =  CURRENT_USER ? CURRENT_USER.token : null;
 
+console.log('TOKEN WITHIN AXIOS REQUESTS IS:');
+console.log(TOKEN);
+
 
 export const publicReq = axios.create({
     baseURL: BASE_URL
@@ -14,5 +17,5 @@ export const publicReq = axios.create({
 
 export const userReq = axios.create({
     baseURL: BASE_URL,
-    header: { token: `Bearer ${TOKEN}` } 
+    headers: { token: `Bearer ${TOKEN}` } 
 })

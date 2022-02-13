2c63a9c45d023746f0099d010930543521035348
Mike Search <search_michael@yahoo.com>
2022-02-13 07:19:46 +0100

    fixes bar chart tick and label display issues

--- admin/src/components/layout/TopSellersChart.js
+++ admin/src/components/layout/TopSellersChart.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import moment from 'moment';
 import styled from 'styled-components';
-import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid, Legend } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid, Label } from 'recharts';
 
 import { userReq } from '../../axiosRequests';
 
@@ -19,7 +19,7 @@ padding: 10px 20px 10px;
 text-align: left;
 width: auto;
 `
-const UserDiv = styled.div`
+const ProductDiv = styled.div`
 border: 1px solid lightgray;
 margin: 2px;
 padding: 10px;
@@ -55,19 +55,8 @@ margin: 5px 10px;
 const TopSellersChart = () => {
 
     const [topData, setTopData ] = useState([]);
-
-    const renderBarChart = (
-        <BarChart width={750} height={250} data={topData}>
-  <CartesianGrid strokeDasharray="3 3" />
-  <XAxis dataKey="title" angle={90} interval={0} />
-  <YAxis dataKey="count" />
-  <Tooltip />
-  {/* <Legend /> */}
-  <Bar dataKey="count" fill="#8884d8" />
-</BarChart>
-    )
-
-   
+    const [barChart, setBarChart] = useState(<BarChart/>);
+    
 
     useEffect(() => {
 
@@ -75,13 +64,8 @@ const TopSellersChart = () => {
             try {
                 const res = await userReq.get('/orders/topsellers');
                 if(res){
-                    console.log('ITEM DATA');
-                    console.log(res.data);
-
                     setTopData(res.data);
-                }
-               
-               
+                } 
             }
             catch (err) { console.log(err) };
 
@@ -97,14 +81,22 @@ const TopSellersChart = () => {
                     return item._id;
                 })
                 const res = await userReq.get(`/products/findall/${ids}`);
-                console.log('Names of TOPSELLERS ARE:');
-                console.log(res.data);
                 const tempData = topData;
                 res.data.map((item, index) => {
-                    tempData[index] = {...tempData[index], title: item.title};
-                    console.log(tempData);
+                    tempData[index] = {...tempData[index], title: item.title.substring(0, 8)};
                 })
-
+                setTopData(tempData);
+                setBarChart(
+                    <BarChart width={750} height={250} data={topData} margin={{ top: 5, right: 20, bottom: 20, left: 0 }}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="title" label={{ value: 'Products', offset: '-10', position: 'insideBottom' }}  />
+              <YAxis dataKey="count" label={{ value: 'Units Sold', angle: -90, position: 'insideLeft' }} />
+              <Tooltip />
+              {/* <Legend /> */}
+              <Bar barSize={25} dataKey="count" fill="#8884d8" />
+            </BarChart>
+                )
+                
             }
             catch (err) {
                 console.log(err)
@@ -113,13 +105,14 @@ const TopSellersChart = () => {
         getNames();
     }, [topData])
 
+
     return (
         <div>
             <Container>
             <TitleDiv>
             <Title>Top Selling Products</Title>
             </TitleDiv>
-            {renderBarChart}
+            {barChart}
             </Container>
 
         </div>

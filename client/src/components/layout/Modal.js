d0432ddf72e5c92740e3200cbb108f27a13e8062
Mike Search <search_michael@yahoo.com>
2022-02-23 08:52:24 +0100

    adds display of details

--- client/src/components/layout/Modal.js
+++ client/src/components/layout/Modal.js
@@ -19,22 +19,55 @@ const ProductBox = styled.div`
 background-color: white;
 border: 5px solid red;
 border-radius: 10% 0 10% 0;
-height: 350px;
+height: auto;
 left: 50%;
 margin-left: -150px;
 margin-top: -175px;
 position: absolute;
+text-align: center;
 top: 50%;
-width: 300px;
+width: auto;
 z-index: 40;
 `
 
+const Form = styled.form`
+margin: 10px 10px 20px;
+`
+
+const Title = styled.h2`
+color: red;
+margin: 20px;
+`
+
+const Image = styled.img`
+margin: 10px;
+max-height: 200px;
+max-width: 200px;
+`
+
+const Select = styled.select`
+border: 2px solid red;
+font-size: 20px;
+margin: 10px;
+padding: 5px;
+`
+
+const Label = styled.label`
+font-size: 20px;
+font-weight: bold;
+`
+
+const Option = styled.option`
+
+`
+
 const Modal = ({showModal, getModalClick, modalContent}) => {
-console.log('SHOW MODAL IS');
-console.log(showModal);
+
 const [display, setDisplay] = useState(false);
-console.log('DISPLAY IS');
-console.log(display);
+console.log('modal content size is');
+console.log(modalContent.size);
+
+
 useEffect(() => {
     setDisplay(showModal);
 }, [showModal]);
@@ -50,7 +83,33 @@ const handleBoxClick = (e) => {
     return (
         <Container style={{display: display ? 'unset': 'none'}} onClick={handleClick}>
             <ProductBox onClick={handleBoxClick}>
-                <h1>{modalContent.title}</h1>
+                <Title>{modalContent.title}</Title>
+                <Image src={modalContent.image}/>
+                <Form>
+                {modalContent !=={} && modalContent.color.length ? 
+                <div>
+                <Label>Color</Label>
+        <Select>
+            {modalContent.color && modalContent.color.map(color => <Option key={color} value={color}>{color[0].toUpperCase() + color.slice(1)}</Option>)}
+        </Select>
+                </div>
+                :
+                null
+                }
+        {modalContent !=={} && modalContent.size.length ?
+            <div>
+        <Label>Size</Label>
+        <Select>
+            {modalContent.size && modalContent.size.map(size => <Option key={size} value={size}>{size[0].toUpperCase() + size.slice(1)}</Option>)}
+        </Select>
+        </div> 
+        :
+        null
+        }
+        
+        
+        
+                </Form>
             </ProductBox>
         </Container>
     )

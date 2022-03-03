d6d24ef934f38a01558f794f0267cc1b2918ebf4
Mike Search <search_michael@yahoo.com>
2022-03-03 08:30:51 +0100

    formats code

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,18 +1,19 @@
 const router = require('express').Router();
 const stripe = require('stripe')(process.env.STRIPE_KEY);
 
+// Stripe Payment Route
 router.post('/payment', (req, res) => {
-stripe.charges.create({
-    source: req.body.tokenId,
-    amount: req.body.amount,
-    currency: 'usd'
-}, (stripeErr, stripeRes) => {
-    if(stripeErr){
-        res.status(500).json({ errors: [{ msg: stripeErr }] });
-    } else {
-        res.status(200).json(stripeRes);
-    }
-})
+    stripe.charges.create({
+        source: req.body.tokenId,
+        amount: req.body.amount,
+        currency: 'usd'
+    }, (stripeErr, stripeRes) => {
+        if (stripeErr) {
+            res.status(500).json({ errors: [{ msg: stripeErr }] });
+        } else {
+            res.status(200).json(stripeRes);
+        }
+    })
 }
 );
 

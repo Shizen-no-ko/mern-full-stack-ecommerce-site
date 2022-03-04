b1eb1d3701e5cd58aa79835da05ea5bab23edc3c
Mike Search <search_michael@yahoo.com>
2022-03-04 08:32:24 +0100

    Comments and formats code

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,13 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
 } from 'redux-persist'
 import storage from 'redux-persist/lib/storage';
 
@@ -15,28 +15,32 @@ import shoppingCartReducer from './shoppingCartRedux';
 import userReducer from './userRedux';
 import errorReducer from './errorRedux';
 
-
+// Exclude 'error' from Persist
 const persistConfig = {
-    key: 'root',
-    version: 1,
-    storage,
-    blacklist: ['error', 'errorMessage']
+  key: 'root',
+  version: 1,
+  storage,
+  blacklist: ['error', 'errorMessage']
 }
 
+// Combine reducers into rootReducer
 const rootReducer = combineReducers({ user: userReducer, cart: shoppingCartReducer, error: errorReducer });
 
+// Apply persistConfig to rootReducer
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+// Store with persist
 export const store = configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({
-        serializableCheck: {
-          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-      }),
-  })
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
 
+// Initial store
 // export const store = configureStore({
 //     reducer: {
 //         cart: shoppingCartReducer,

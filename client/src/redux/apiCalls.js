a436e5ebc99b9b251a20fa3a556e74a05669b222
Mike Search <search_michael@yahoo.com>
2022-03-04 08:53:21 +0100

    Tidies-up, comments and formats code

--- client/src/redux/apiCalls.js
+++ client/src/redux/apiCalls.js
@@ -3,10 +3,13 @@ import { clearCart } from './shoppingCartRedux';
 import { success, failure } from './errorRedux';
 import { publicReq, userReq } from "../axiosRequests";
 
+
+// Login
 export const login = async (dispatch, user) => {
     dispatch(loginStart());
-    try{
-        // Uses 'main' param to enable condition in auth login route
+    try {
+        // Uses 'main' param to enable condition in auth login route for main webpage
+        // as opposed to 'admin' for admin webpage
         const res = await publicReq.post('auth/login/main', user);
         dispatch(success());
         dispatch(loginSuccess(res.data));
@@ -14,27 +17,30 @@ export const login = async (dispatch, user) => {
     catch (err) {
         dispatch(failure(err.response.data.errors));
         dispatch(loginFailure());
-      
+
     }
 };
 
+
+// Register
 export const register = async (dispatch, user) => {
     dispatch(loginStart());
-    try{
+    try {
         const res = await publicReq.post('auth/register', user);
-        console.log(`res.data is ${res.data}`);
         dispatch(success());
         dispatch(loginSuccess(res.data));
     }
     catch (err) {
         dispatch(failure(err.response.data.errors));
-        dispatch(loginFailure()); 
+        dispatch(loginFailure());
     }
 };
 
+
+// For updating user in like button functionality
 export const update = async (dispatch, user) => {
     dispatch(updateUser());
-    try{
+    try {
         const res = await userReq.get(`users/updateCurrent/${user.user._id}`);
         dispatch(updateUser(res.data));
     }
@@ -43,8 +49,9 @@ export const update = async (dispatch, user) => {
     }
 };
 
+// Add order to DB and clear shopping cart
 export const order = async (dispatch, orderData) => {
-    try{
+    try {
         const res = await userReq.post('orders/add', orderData);
         dispatch(clearCart());
     }

141b125158bae850bdbd61f17ba00f93184d86e4
Mike Search <search_michael@yahoo.com>
2022-03-04 07:57:00 +0100

    Comments and formats code

--- middleware/tokenAuth.js
+++ middleware/tokenAuth.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
 
+// Token check middleware
 const tokenAuth = (req, res, next) => {
     const token = req.headers.token;
-    
     if (!token) {
         return res.status(401).json({ errors: [{ msg: "No token" }] });
     }
-
+    // Substring removes 'Bearer' text from token
     jwt.verify(token.substring(7), process.env.JWT_SECRET, (err, decoded) => {
         // handles malformed tokens
         if (err) {

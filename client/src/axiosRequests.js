a534a86920b6b03cc09d8a2b49afb7bb0c984bf5
Mike Search <search_michael@yahoo.com>
2022-03-04 08:09:21 +0100

    Removes commented-out code, comments and formats code

--- client/src/axiosRequests.js
+++ client/src/axiosRequests.js
@@ -2,16 +2,18 @@ import axios from 'axios';
 
 
 const BASE_URL = 'http://localhost:5000/api';
-// const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.token;
 
+// Handle condition of whether Current User is set in Redux store, or not, and set token accordingly
 const CURRENT_USER = localStorage.length > 0 ? JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser : null;
-const TOKEN =  CURRENT_USER ? CURRENT_USER.token : 'null';
+const TOKEN = CURRENT_USER ? CURRENT_USER.token : 'null';
 
+// Public requests
 export const publicReq = axios.create({
     baseURL: BASE_URL
 });
 
+// User requests
 export const userReq = axios.create({
     baseURL: BASE_URL,
-    headers: { token: `Bearer ${TOKEN}` } 
+    headers: { token: `Bearer ${TOKEN}` }
 })

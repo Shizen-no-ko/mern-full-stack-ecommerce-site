8ff026f927d46f146c07515be4c2cf3bf8e389b2
Mike Search <search_michael@yahoo.com>
2022-03-04 08:56:33 +0100

    Tidies-up, comments and formats code

--- client/src/data/data.js
+++ client/src/data/data.js
@@ -1,18 +1,26 @@
+// Hardwired data for homepage
+
 export const sliderData = [
-    { title: "Pink Sake Set",
-    description: "Beautiful, gradutated pink, sake drinking set with heart motif",
-    img: "https://source.unsplash.com/yPYNhYr7Xrs/1920x1280",
-    price: "$500"},
+    {
+        title: "Pink Sake Set",
+        description: "Beautiful, gradutated pink, sake drinking set with heart motif",
+        img: "https://source.unsplash.com/yPYNhYr7Xrs/1920x1280",
+        price: "$500"
+    },
 
-    { title: "Ball Teapot",
-    description: "Cute ball-shaped teapot in various colors and finishes",
-    img: "https://source.unsplash.com/7EeeSN-eGsI/1920x1280",
-    price: "$250"}, 
+    {
+        title: "Ball Teapot",
+        description: "Cute ball-shaped teapot in various colors and finishes",
+        img: "https://source.unsplash.com/7EeeSN-eGsI/1920x1280",
+        price: "$250"
+    },
 
-    { title: "Paring Knife",
-    description: "High quality, durable, hand-wrought paring knife",
-    img: "https://source.unsplash.com/wgmFnnLz2gw/1920x1280",
-    price: "$250"}
+    {
+        title: "Paring Knife",
+        description: "High quality, durable, hand-wrought paring knife",
+        img: "https://source.unsplash.com/wgmFnnLz2gw/1920x1280",
+        price: "$250"
+    }
 ];
 
 export const categoriesData = [
@@ -33,47 +41,4 @@ export const categoriesData = [
     }
 ];
 
-
-export const productData = [
-    {
-        id:1,
-        img: "https://source.unsplash.com/3g5AfQZWUCA/1920x1280",
-        category:["iro-iro"]
-    },
-    {
-        id:2,
-        img: "https://source.unsplash.com/_5Q1-ae44aw/1920x1280",
-        category:["iro-iro"]
-    },
-    {
-        id:3,
-        img: "https://source.unsplash.com/FaUCROC_BZ8/1920x1280",
-        category:["homeware"]
-    },
-    {
-        id:4,
-        img: "https://source.unsplash.com/odyfTDRZxvY/1920x1280",
-        category:["clothing"]
-    },
-    {
-        id:5, 
-        img: "https://source.unsplash.com/kbBaNkxLdHY/1920x1280",
-        category:["homeware"]
-    },
-    {
-        id:6,
-        img: "https://source.unsplash.com/Ayy_K-y_dLA/1920x1280",
-        category:["iro-iro"]
-    },
-    {
-        id:7,
-        img: "https://source.unsplash.com/VKI8S_O-pYg/1920x1280",
-        category:["homeware", "iro-iro" ]
-    },
-    {
-        id:8,
-        img: "https://source.unsplash.com/JcXfveuRYig/1920x1280",
-        category:["iro-iro"]
-    }
-]
 

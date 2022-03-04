670d71b5d261b61ce0f14adae5735bb7b0bbce97
Mike Search <search_michael@yahoo.com>
2022-03-04 08:23:52 +0100

    Tidies-up, comments and formats code

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import './App.css';
 
+// Scroll to top of page on history change
 import ScrollToTop from './utilities/ScrollToTop';
 
 import Landing from './components/pages/Landing';
@@ -17,27 +18,24 @@ import Success from './components/pages/Success';
 
 function App() {
   const user = useSelector(state => state.user.currentUser);
-  
 
   return (
     <Router>
- <Fragment>
- <ScrollToTop/>
-   <Route exact path="/" component={Landing} />
-   <Switch>
-   <Route exact path="/login">{user ? <Redirect to='/'/> : <Login/>}</Route>
-   <Route exact path="/register">{user ? <Redirect to='/'/> : <Register/>}</Route>
-   <Route exact path="/cart" component={ShoppingCart} />
-   <Route exact path="/product/:id" component={IndividualProduct} />
-   <Route exact path="/pay" component={Pay} />
-   <Route exact path="/success" component={Success} />
-   <Route exact path="/products/:category" component={ProductsFilter} />
-   <Route exact path="/products" component={ProductsFilter} />
-   {/* :category */}
-   </Switch>
-    </Fragment>
+      <Fragment>
+        <ScrollToTop />
+        <Route exact path="/" component={Landing} />
+        <Switch>
+          <Route exact path="/login">{user ? <Redirect to='/' /> : <Login />}</Route>
+          <Route exact path="/register">{user ? <Redirect to='/' /> : <Register />}</Route>
+          <Route exact path="/cart" component={ShoppingCart} />
+          <Route exact path="/product/:id" component={IndividualProduct} />
+          <Route exact path="/pay" component={Pay} />
+          <Route exact path="/success" component={Success} />
+          <Route exact path="/products/:category" component={ProductsFilter} />
+          <Route exact path="/products" component={ProductsFilter} />
+        </Switch>
+      </Fragment>
     </Router>
-   
   );
 }
 

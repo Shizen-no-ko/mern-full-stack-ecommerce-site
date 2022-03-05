784d81c7a7313975341d33e57c08711a658a81bb
Mike Search <search_michael@yahoo.com>
2022-03-05 07:50:37 +0100

    Tidies-up, comments and formats code

--- client/src/components/pages/Register.js
+++ client/src/components/pages/Register.js
@@ -1,11 +1,10 @@
-import {useRef, useState} from 'react';
+import { useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { persistor } from '../../redux/store';
 
 import styled from 'styled-components';
 import { mobile, portraitTablet, landscapeTablet } from '../../responsive';
 import { register } from '../../redux/apiCalls';
-// import { clearErrors } from '../../redux/userRedux';
 
 import Navbar from '../layout/Navbar';
 import SubscriptionForm from '../layout/SubscriptionForm';
@@ -53,9 +52,10 @@ ${portraitTablet({
 
 ${landscapeTablet({
     fontSize: '22px',
-   width: '60%'
+    width: '60%'
 })};
 `
+
 const Title = styled.h1`
 color: rgba(255, 0, 0, 0.9);
 font-size: 3rem;
@@ -72,15 +72,11 @@ ${portraitTablet({
 `
 
 const Form = styled.form`
-${'' /* border: 1px solid white; */}
 border-radius: 20px;
-${'' /* box-shadow: 5px 5px 15px rgb(255,192,203); */}
 display: flex;
 flex-direction: column;
 padding: 20px 15px;
 `
-
-
 
 const Input = styled.input`
 border-radius: 10px;
@@ -91,15 +87,16 @@ padding: 10px;
 width: 94%;
 
 ${mobile({
-   fontSize: '15px',
-   marginLeft: '0px'
+    fontSize: '15px',
+    marginLeft: '0px'
 })};
 
 ${portraitTablet({
-   fontSize: '20px',
-   marginLeft: '0px'
+    fontSize: '20px',
+    marginLeft: '0px'
 })};
 `
+
 const PasswordContainer = styled.div`
 align-items: center;
 display: flex;
@@ -112,11 +109,11 @@ margin-left: -45px;
 width: 30px;
 
 ${mobile({
-   fontSize: '15px'
+    fontSize: '15px'
 })};
 
 ${portraitTablet({
-   fontSize: '20px'
+    fontSize: '20px'
 })};
 `
 
@@ -128,11 +125,11 @@ margin-left: -45px;
 width: 30px;
 
 ${mobile({
-   fontSize: '15px'
+    fontSize: '15px'
 })};
 
 ${portraitTablet({
-   fontSize: '20px'
+    fontSize: '20px'
 })};
 `
 
@@ -141,7 +138,6 @@ all: unset;
 background-color: white;
 border: 4px solid red;
 border-radius: 20px 0;
-${'' /* border-radius: 10px; */}
 color: red;
 cursor: pointer;
 font-size: 20px;
@@ -151,8 +147,6 @@ outline: none;
 padding: 10px;
 text-align: center;
 width: 50%;
-
-
 
 &:hover{
     background-color: red;
@@ -168,14 +162,15 @@ width: 50%;
 
 ${mobile({
     fontSize: '17px',
-   width: '80%'
+    width: '80%'
 })};
 
 ${portraitTablet({
     fontSize: '22px',
-   width: '80%'
+    width: '80%'
 })};
 `
+
 const PrivacyPolicy = styled.div`
 margin: 20px auto 0;
 text-align: center;
@@ -202,81 +197,77 @@ margin: 10px;
 text-align: center;
 `
 
-const Register = (props) => {
 
-const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    passwordConfirm:''
-});
+const Register = () => {
 
-const { username, email, password, passwordConfirm } = formData;
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        passwordConfirm: ''
+    });
 
-const dispatch = useDispatch();
-// dispatch(clearErrors());
-const { isFetching } = useSelector(state => state.user);
-const { error, errorMessage } = useSelector(state => state.error);
+    const { username, email, password, passwordConfirm } = formData;
 
-const onChange = (e) => setFormData({...formData, [e.target.name]: e.target.value});
+    const dispatch = useDispatch();
 
-const onSubmit = (e) => {
-    e.preventDefault();
-    // if (password !== passwordConfirm){
-    //     console.log("Passwords Do Not Match")
-    // }else{
-    //     console.log(formData)
-    // }
-};
+    const { error, errorMessage } = useSelector(state => state.error);
 
-const handleClick = (e) => {
-    e.preventDefault();
-    persistor.purge();
-    register(dispatch, { username, email, password, passwordConfirm })
-};
+    const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
 
-const eyeRef1 = useRef(null);
-const closedEyeRef1 = useRef(null);
-const eyeRef2 = useRef(null);
-const closedEyeRef2 = useRef(null);
-const passwordRef1 = useRef(null);
-const passwordRef2 = useRef(null);
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
 
-const refsArray = [[eyeRef1, closedEyeRef1, passwordRef1], [eyeRef2, closedEyeRef2, passwordRef2]]
+    const handleClick = (e) => {
+        e.preventDefault();
+        persistor.purge();
+        register(dispatch, { username, email, password, passwordConfirm })
+    };
 
-const openEye = (pos) => {
-    refsArray[pos][1].current.style.display = 'unset';
-    refsArray[pos][0].current.style.display = 'none';
-    refsArray[pos][2].current.type = 'text';
-};
+    // Refs for password hide/display functionality
+    const eyeRef1 = useRef(null);
+    const closedEyeRef1 = useRef(null);
+    const eyeRef2 = useRef(null);
+    const closedEyeRef2 = useRef(null);
+    const passwordRef1 = useRef(null);
+    const passwordRef2 = useRef(null);
 
-const closeEye = (pos) => {
-    refsArray[pos][1].current.style.display = 'none';
-    refsArray[pos][0].current.style.display = 'unset';
-    refsArray[pos][2].current.type = 'password';
-};
+    const refsArray = [[eyeRef1, closedEyeRef1, passwordRef1], [eyeRef2, closedEyeRef2, passwordRef2]]
 
-console.log(`ERROR IS ${JSON.stringify(errorMessage)} `);
+    // pos argument to select which eye
+    const openEye = (pos) => {
+        refsArray[pos][1].current.style.display = 'unset';
+        refsArray[pos][0].current.style.display = 'none';
+        refsArray[pos][2].current.type = 'text';
+    };
+
+    // pos argument to select which eye
+    const closeEye = (pos) => {
+        refsArray[pos][1].current.style.display = 'none';
+        refsArray[pos][0].current.style.display = 'unset';
+        refsArray[pos][2].current.type = 'password';
+    };
+
 
     return (
         <div>
             <Navbar />
-
             <Container>
-            <Wrapper>
-            <Title>Create Your Account</Title>
+                <Wrapper>
+                    <Title>Create Your Account</Title>
                     <Form onSubmit={e => onSubmit(e)}>
-                    <Input onChange={e => onChange(e)} required name='username' type='text' value={username} placeholder='Your Name'></Input>
+                        <Input onChange={e => onChange(e)} required name='username' type='text' value={username} placeholder='Your Name'></Input>
                         <Input onChange={e => onChange(e)} required name='email' type='email' value={email} placeholder='Your Email'></Input>
                         <PasswordContainer>
-                        <Input onChange={e => onChange(e)} ref={passwordRef1} name='password' type='password' value={password} placeholder='Choose a Password'></Input>
-                        <Eye onClick={() => openEye(0)} ref={eyeRef1} ><i className="fas fa-eye"></i></Eye>
-                        <ClosedEye onClick={() => closeEye(0)} ref={closedEyeRef1}><i className="fas fa-eye-slash"></i></ClosedEye>
+                            <Input onChange={e => onChange(e)} ref={passwordRef1} name='password' type='password' value={password} placeholder='Choose a Password'></Input>
+                            <Eye onClick={() => openEye(0)} ref={eyeRef1} ><i className="fas fa-eye"></i></Eye>
+                            <ClosedEye onClick={() => closeEye(0)} ref={closedEyeRef1}><i className="fas fa-eye-slash"></i></ClosedEye>
                         </PasswordContainer>
                         <PasswordContainer>
-                        <Input onChange={e => onChange(e)} ref={passwordRef2} name='passwordConfirm' type='password' value={passwordConfirm} placeholder='Re-enter Password'></Input>
-                        <Eye onClick={() => openEye(1)} ref={eyeRef2}><i className="fas fa-eye"></i></Eye>
-                        <ClosedEye onClick={() => closeEye(1)} ref={closedEyeRef2}><i className="fas fa-eye-slash"></i></ClosedEye>
+                            <Input onChange={e => onChange(e)} ref={passwordRef2} name='passwordConfirm' type='password' value={passwordConfirm} placeholder='Re-enter Password'></Input>
+                            <Eye onClick={() => openEye(1)} ref={eyeRef2}><i className="fas fa-eye"></i></Eye>
+                            <ClosedEye onClick={() => closeEye(1)} ref={closedEyeRef2}><i className="fas fa-eye-slash"></i></ClosedEye>
                         </PasswordContainer>
                         <PrivacyPolicy>By creating this account, I consent to the processing of my personal data in accordance with the <b>Privacy Policy</b></PrivacyPolicy>
                         <Button onClick={handleClick}>Sign Me Up</Button>

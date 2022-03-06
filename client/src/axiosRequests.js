f6621fe3cd878cbdf9a8baabcad81ab69ea5b00f
Mike Search <search_michael@yahoo.com>
2022-03-06 09:12:23 +0100

    Adds axios interceptor, to solve setting of headers too early, causing headers errors in backend issue

--- client/src/axiosRequests.js
+++ client/src/axiosRequests.js
@@ -1,19 +1,30 @@
 import axios from 'axios';
 
-
 const BASE_URL = 'http://localhost:5000/api';
 
-// Handle condition of whether Current User is set in Redux store, or not, and set token accordingly
-const CURRENT_USER = localStorage.length > 0 ? JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser : null;
-const TOKEN = CURRENT_USER ? CURRENT_USER.token : 'null';
+
+// User requests
+const userReq = axios.create({ baseURL: BASE_URL });
+// Handle condition of whether Current User is set in Redux store, or not, 
+//and set token accordingly using axios interceptors
+userReq.interceptors.request.use(
+    async (config) => {
+        const CURRENT_USER = localStorage.length > 0 ? JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser : null;
+        const TOKEN = CURRENT_USER ? CURRENT_USER.token : 'null';
+        config.headers = {
+            token: `Bearer ${TOKEN}`,
+        };
+        return config;
+    },
+    (error) => {
+        Promise.reject(error);
+    }
+);
 
 // Public requests
-export const publicReq = axios.create({
+const publicReq = axios.create({
     baseURL: BASE_URL
 });
 
-// User requests
-export const userReq = axios.create({
-    baseURL: BASE_URL,
-    headers: { token: `Bearer ${TOKEN}` }
-})
+
+export { publicReq, userReq }

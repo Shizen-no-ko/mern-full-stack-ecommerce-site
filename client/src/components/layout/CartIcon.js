46b286e307a5200327e884889589a81eff10bae6
Mike Search <search_michael@yahoo.com>
2022-03-16 07:39:05 +0100

    Tidies-up, comments and formats code

--- client/src/components/layout/CartIcon.js
+++ client/src/components/layout/CartIcon.js
@@ -1,28 +1,27 @@
 import { useSelector } from 'react-redux';
-
 import styled from 'styled-components';
-import Badge from './Badge';
 
-import {mobile} from '../../responsive';
+import { mobile } from '../../responsive';
+
+import Badge from './Badge';
 
 const CartContainer = styled.div`
 font-size:20px;
 padding-top: 5px;
 
 ${mobile({
-        fontSize: '13px'
-        })};
-
+    fontSize: '13px'
+})};
 `
 
-
-const CartIcon = (props) => {
-    const itemCount = useSelector(state=>state.cart.itemCount);
-    return(
-       <CartContainer>
-        <Badge cartItems={itemCount} color={"red"}/>
-           <i className="fas fa-shopping-cart"></i>
-       </CartContainer>
+// Displays cart icon with item count badge passing itemCount state from Redux into badge
+const CartIcon = () => {
+    const itemCount = useSelector(state => state.cart.itemCount);
+    return (
+        <CartContainer>
+            <Badge cartItems={itemCount} color={"red"} />
+            <i className="fas fa-shopping-cart"></i>
+        </CartContainer>
     )
 }
 

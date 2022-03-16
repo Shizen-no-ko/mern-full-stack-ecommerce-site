90fe734f6e8bac77973436b5baa789eacee554ef
Mike Search <search_michael@yahoo.com>
2022-03-16 10:07:02 +0100

    Tidies-up and formats code

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import './App.css';
@@ -16,27 +16,24 @@ import IndividualOrder from './components/pages/IndividualOrder';
 
 function App() {
   const user = useSelector(state => state.user.currentUser);
-  // Protect all routes
   return (
     <Router>
- <Fragment>
- <ScrollToTop/>
-   <Route exact path="/">{user ? <Landing/> : <Login/>}</Route>
-   <Switch>
-   <Route exact path="/login">{user ? <Redirect to='/'/> : <Login/>}</Route>
-   <Route exact path="/product/:id">{user ? <IndividualProduct/> : <Login/>}</Route> 
-   <Route exact path="/products/:category">{user ? <ProductsFilter/> : <Login/>}</Route> 
-   <Route exact path="/products">{user ? <ProductsFilter/> : <Login/>}</Route>
-   <Route exact path="/deleted">{user ? <DeletedProducts/> : <Login/>}</Route>
-   <Route exact path="/add">{user ? <IndividualProduct add='true' /> : <Login/>}</Route>
-   <Route exact path="/statistics">{user ? <Stats /> : <Login/>}</Route>
-   <Route exact path="/order/:id">{user ? <IndividualOrder/> : <Login/>}</Route> 
-   {/* component={() => (<IndividualProduct add='true' />)}/>  */}
-   {/* :category */}
-   </Switch>
-    </Fragment>
+      <Fragment>
+        <ScrollToTop />
+        <Route exact path="/">{user ? <Landing /> : <Login />}</Route>
+        <Switch>
+          <Route exact path="/login">{user ? <Redirect to='/' /> : <Login />}</Route>
+          <Route exact path="/product/:id">{user ? <IndividualProduct /> : <Login />}</Route>
+          <Route exact path="/products/:category">{user ? <ProductsFilter /> : <Login />}</Route>
+          <Route exact path="/products">{user ? <ProductsFilter /> : <Login />}</Route>
+          <Route exact path="/deleted">{user ? <DeletedProducts /> : <Login />}</Route>
+          <Route exact path="/add">{user ? <IndividualProduct add='true' /> : <Login />}</Route>
+          <Route exact path="/statistics">{user ? <Stats /> : <Login />}</Route>
+          <Route exact path="/order/:id">{user ? <IndividualOrder /> : <Login />}</Route>
+        </Switch>
+      </Fragment>
     </Router>
-   
+
   );
 }
 

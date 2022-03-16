aa6117b5cf7d3c2824077d7a945eb74d0e2aa083
Mike Search <search_michael@yahoo.com>
2022-03-16 07:24:49 +0100

    Tidies-up, comments and formats code

--- client/src/components/layout/Categories.js
+++ client/src/components/layout/Categories.js
@@ -1,12 +1,11 @@
 import styled from 'styled-components';
-import {mobile, portraitTablet, landscapeTablet} from '../../responsive';
-
+import { mobile, portraitTablet, landscapeTablet } from '../../responsive';
 
 import CategoryElement from './CategoryElement';
-import {categoriesData} from '../../data/data.js';
+import { categoriesData } from '../../data/data.js';
+
 
 const Container = styled.div`
-${'' /* background-color: red; */}
 display:flex;
 justify-content: space-between;
 height: 60vh;
@@ -14,34 +13,32 @@ margin: 40px auto;
 width: 97vw;
 
 ${mobile({
-        flexDirection: 'column',
-        height: 'auto'
-        })};
+    flexDirection: 'column',
+    height: 'auto'
+})};
 
-        ${portraitTablet({
-        flexDirection: 'column',
-        height: 'auto'
-        })};
+${portraitTablet({
+    flexDirection: 'column',
+    height: 'auto'
+})};
 
-        ${landscapeTablet({
-        height: '200px'
-        })};
-
+${landscapeTablet({
+    height: '200px'
+})};
 `
 
+// Map categories onto landing page
 const Categories = () => {
-    return(
-       <Container>
-       {
-        categoriesData.map((category, i) => { 
-            return(
-                <CategoryElement key={i} element={category}/>   
-                )
-        
-       })
-       }
-       
-       </Container>
+    return (
+        <Container>
+            {
+                categoriesData.map((category, i) => {
+                    return (
+                        <CategoryElement key={i} element={category} />
+                    )
+                })
+            }
+        </Container>
     )
 }
 

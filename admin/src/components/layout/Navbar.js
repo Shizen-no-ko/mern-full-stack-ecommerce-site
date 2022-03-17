6a80f23295f45c85448407c47f93a38f9b6be9ba
Mike Search <search_michael@yahoo.com>
2022-03-17 05:27:22 +0100

    Creates SearchBar using code from client, brings it into Navbar

--- admin/src/components/layout/Navbar.js
+++ admin/src/components/layout/Navbar.js
@@ -3,40 +3,38 @@ import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { persistor } from '../../redux/store';
 import styled from 'styled-components';
-import CartIcon from './CartIcon';
-import Messaging from './Messaging';
-// import { logout } from '../../redux/userRedux';
-import {mobile, portraitTablet, landscapeTablet} from '../../responsive';
+
+import SearchBar from './SearchBar';
+
+import { mobile, portraitTablet, landscapeTablet } from '../../responsive';
 
 
 const Container = styled.div`
     height: 80px;
 
-    ${mobile({
-        height: '120px'
-        })};
+${mobile({
+    height: '120px'
+})};
 
-        ${portraitTablet({
-        height: '120px'
-        })};
+${portraitTablet({
+    height: '120px'
+})};
 `
 
 const Wrapper = styled.div`
 align-items: center;
-${'' /* border-bottom: 1px solid red; */}
 display: flex;
 justify-content: space-between;
 padding: 10px 20px;
 
 ${mobile({
-    flexDirection:'column',
-        })}
+    flexDirection: 'column',
+})}
 
-        ${portraitTablet({
-        flexDirection: "column",
-        marginTop: '20px'
-        })};
-
+${portraitTablet({
+    flexDirection: "column",
+    marginTop: '20px'
+})};
 `
 
 const Search = styled.div`
@@ -45,8 +43,6 @@ font-size: 20px;
 justify-content: flex-start;
 line-height: 25px;
 display: flex;
-
-
 `
 
 
@@ -56,16 +52,14 @@ justify-content: space-evenly;
 display: flex;
 
 ${mobile({
-        justifyContent: 'space-between',
-        
-        })};
+    justifyContent: 'space-between',
 
-        ${portraitTablet({
-            
-        justifyContent: 'space-between'
-        })};
+})};
 
-      
+${portraitTablet({
+
+    justifyContent: 'space-between'
+})};     
 `
 
 const StyledLink = styled(Link)`
@@ -75,7 +69,6 @@ text-decoration: none;
 &:active{
     color: red;
 }
-
 `
 
 const MenuItem = styled.div`
@@ -83,27 +76,24 @@ cursor: pointer;
 font-size: 18px;
 position: relative;
 
-
-
 &:hover{
    transform: scale(110%);
 }
 
 ${mobile({
-        fontSize: '12px',
-        margin: '7px 5px'
-        })};
+    fontSize: '12px',
+    margin: '7px 5px'
+})};
 
-        ${portraitTablet({
-        fontSize: '20px',
-        margin: '12px 10px'
-        })};
+${portraitTablet({
+    fontSize: '20px',
+    margin: '12px 10px'
+})};
 
-        ${landscapeTablet({
-            fontSize: '15px',
-        margin: '10px'
-        })};
-
+${landscapeTablet({
+    fontSize: '15px',
+    margin: '10px'
+})};
 `
 
 
@@ -112,8 +102,8 @@ margin: 0 15px;
 text-align: center;
 
 ${landscapeTablet({
-            margin: '0 auto 0 15px'
-        })};
+    margin: '0 auto 0 15px'
+})};
 `
 
 const Logo = styled.h1`
@@ -121,49 +111,49 @@ color: red;
 margin: 0 15px;
 
 ${mobile({
-        fontSize: '20px',
-        margin: '0 5px'
-        })};
+    fontSize: '20px',
+    margin: '0 5px'
+})};
 
-        ${portraitTablet({
-        fontSize: '35px',
-        margin: '0 10px'
-        })};
+${portraitTablet({
+    fontSize: '35px',
+    margin: '0 10px'
+})};
 
-        ${landscapeTablet({
-        fontSize: '35px',
-        margin: '0 10px 0 auto'
-        })};
-
+${landscapeTablet({
+    fontSize: '35px',
+    margin: '0 10px 0 auto'
+})};
 `
+
 const LogoText = styled.h1`
 margin: 0;
 
 ${mobile({
-        fontSize: '2rem'
-        })};
+    fontSize: '2rem'
+})};
 
-        ${portraitTablet({
-        fontSize: "2.5rem"
-        })};
+${portraitTablet({
+    fontSize: "2.5rem"
+})};
 
-        ${landscapeTablet({
-        fontSize: "2rem"
-        })};
-
+${landscapeTablet({
+    fontSize: "2rem"
+})};
 `
+
 const Subtitle = styled.h5`
 margin: 0;
 
 ${mobile({
-        display: 'none'
-        })};
+    display: 'none'
+})};
 
-        ${portraitTablet({
-        display: "none"
-        })};
-
+${portraitTablet({
+    display: "none"
+})};
 `
+
 const Input = styled.input`
 border: 1px solid lightgray;
 cursor: pointer;
@@ -183,9 +173,8 @@ padding: 5px;
 
 ${landscapeTablet({
     fontSize: '15px',
-       width: '150px'
-        })};
-
+    width: '150px'
+})};
 `
 
 const SearchIcon = styled.div`
@@ -193,120 +182,94 @@ margin-left: -30px;
 
 ${mobile({
     marginLeft: '-25px'
-        })};
+})};
 
-        ${landscapeTablet({
+${landscapeTablet({
     fontSize: '15px',
-        })};
-
+})};
 `
-
 
 const LZone = styled.div`
 flex: 0.5;
 
 ${mobile({
     display: 'none'
-        })};
+})};
 
-        ${portraitTablet({
-        display: "none"
-        })};
-
-
+${portraitTablet({
+    display: "none"
+})};
 `
+
 const CZone = styled.div`
 align-items: center;
 display: flex;
 flex: 1;
 
 ${mobile({
-        flexDirection: 'column'
-        })};
+    flexDirection: 'column'
+})};
 
-        ${landscapeTablet({
-        flex: '2',
-        })};
-
+${landscapeTablet({
+    flex: '2',
+})};
 `
+
 const RZone = styled.div`
 flex: 1;
 
 ${mobile({
-       width: '60%'
-        })};
+    width: '60%'
+})};
 
-        ${portraitTablet({
-       width: '60%'
-        })};
+${portraitTablet({
+    width: '60%'
+})};
 
-        ${landscapeTablet({
-        flex: '1.25'
-        })};
-
-
+${landscapeTablet({
+    flex: '1.25'
+})};
 `
 
 const Navbar = () => {
 
-    // const dispatch = useDispatch();
-
-    const [ searchState, setSearchState ] = useState("");
     const user = useSelector(state => state.user.currentUser);
- 
-
-    const handleChange = (e) => {
-        setSearchState(e.target.value);
-    }
 
     const handleLogout = async () => {
         await persistor.purge();
-        // logout(dispatch());
         window.location.href = '/';
     }
 
     return (
         <div>
-
-        
-        <Container>
-            <Wrapper>
-                <LZone>
-                    <Search>
-                        <Input
-                            type="text"
-                            value={searchState}
-                            onChange={handleChange}
-                            // placeholder='Search' 
-                            />
-                        <SearchIcon><i className="fas fa-search"></i></SearchIcon>
-                    </Search>
-                </LZone>
-                <StyledLink to='/'>
-                <CZone>
-                <Logo> <i className="fas fa-torii-gate"></i></Logo>
-                    <LogoTextContainer>
-                        <LogoText>
-                            Nihon no Mono
-                        </LogoText>
-                        <Subtitle>Admin</Subtitle>
-                    </LogoTextContainer>
-                </CZone>
-                </StyledLink>
-                <RZone>
-                    <Menu>
-                        {!user && <MenuItem><StyledLink to='/login'>LOG IN</StyledLink></MenuItem>}
-                        {user && <MenuItem><StyledLink to='/add'>ADD</StyledLink></MenuItem>}
-                        {user && <MenuItem><StyledLink to='/'>PRODUCTS</StyledLink></MenuItem>}
-                        {user && <MenuItem><StyledLink to='/statistics'>STATS</StyledLink></MenuItem>}
-                        {user && <MenuItem><StyledLink to='/deleted'>DELETED</StyledLink></MenuItem>}
-                        {user && <MenuItem ><StyledLink onClick={handleLogout} to='/'>LOGOUT</StyledLink></MenuItem>}
-                        {/* <MenuItem><StyledLink to='/cart'><CartIcon /></StyledLink></MenuItem> */}
-                    </Menu>
-                </RZone>
-            </Wrapper>
-        </Container>
-{/* <Messaging/> */}
+            <Container>
+                <Wrapper>
+                    <LZone>
+                        <SearchBar />
+                    </LZone>
+                    <StyledLink to='/'>
+                        <CZone>
+                            <Logo> <i className="fas fa-torii-gate"></i></Logo>
+                            <LogoTextContainer>
+                                <LogoText>
+                                    Nihon no Mono
+                                </LogoText>
+                                <Subtitle>Admin</Subtitle>
+                            </LogoTextContainer>
+                        </CZone>
+                    </StyledLink>
+                    <RZone>
+                        <Menu>
+                            {!user && <MenuItem><StyledLink to='/login'>LOG IN</StyledLink></MenuItem>}
+                            {user && <MenuItem><StyledLink to='/add'>ADD</StyledLink></MenuItem>}
+                            {user && <MenuItem><StyledLink to='/'>PRODUCTS</StyledLink></MenuItem>}
+                            {user && <MenuItem><StyledLink to='/statistics'>STATS</StyledLink></MenuItem>}
+                            {user && <MenuItem><StyledLink to='/deleted'>DELETED</StyledLink></MenuItem>}
+                            {user && <MenuItem ><StyledLink onClick={handleLogout} to='/'>LOGOUT</StyledLink></MenuItem>}
+                        </Menu>
+                    </RZone>
+                </Wrapper>
+            </Container>
         </div>
     )
 }
---
+++ admin/src/components/layout/SearchBar.js
@@ -0,0 +1,265 @@
+import { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
+
+import { userReq } from '../../axiosRequests.js';
+import { mobile, landscapeTablet } from '../../responsive';
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+`
+
+const Search = styled.div`
+align-items: center;
+font-size: 20px;
+justify-content: flex-start;
+line-height: 25px;
+display: flex;
+`
+
+const Input = styled.input`
+border: 1px solid lightgray;
+cursor: pointer;
+font-size: 20px;
+padding: 5px;
+  margin: 5px;
+  width: 28vw;
+
+&:focus{
+    border: 1px solid red;
+    outline: none;
+}
+
+&:hover{
+    border: 1px solid black;
+
+}
+
+${landscapeTablet({
+    fontSize: '15px',
+
+})};
+`
+
+const SearchIcon = styled.div`
+margin-left: -30px;
+
+${mobile({
+    marginLeft: '-25px'
+})};
+
+        ${landscapeTablet({
+    fontSize: '15px',
+})};
+`
+
+const DropDown = styled.div`
+background-color: white;
+display: flex;
+flex-direction: column;
+justify-content: space-evenly;
+left: 24px;
+position: absolute;
+top: 60px;
+width: 28vw;
+z-index: 20;
+`
+
+const DropElement = styled.li`
+background-color: white;
+border: 1px solid lightgray;
+font-size: 20px;
+list-style-type: none;
+margin: 1px;
+overflow: hidden;
+padding: 5px 10px;
+
+&:hover {
+    background-color: pink;
+}
+`
+
+
+const SearchBar = () => {
+
+    const history = useHistory();
+
+    const [searchState, setSearchState] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
+    const [keyWords, setKeyWords] = useState('');
+    const [dropText, setDropText] = useState([]);
+    const [dropListState, setDropListState] = useState({
+        cursor: -1,
+        value: ''
+    });
+
+    // resets all states upon redirect or blur
+    const resetStates = () => {
+        setDropListState({
+            cursor: -1,
+            value: ''
+        });
+        setDropText([]);
+        setSearchState('');
+        setSearchTerm('');
+    };
+
+    // controlled input state
+    const handleChange = (e) => {
+        setSearchState(e.target.value);
+    };
+
+    // direct click on dropdown suggestion
+    const handleClick = (word) => {
+        setSearchState(word[0]);
+        console.log(word);
+        setSearchState('');
+        history.push(`/products/search?field=${word[1]}&value=${word[0]}`);
+    };
+
+    // Handle key navigation of dropdown
+    const handleKeyDown = (e) => {
+
+        // down arrow
+        if (e.keyCode === 40) {
+            setDropListState(prevState => ({
+                cursor: prevState.cursor < dropText.length - 1 ? prevState.cursor + 1 : prevState.cursor,
+                value: dropText[prevState.cursor < dropText.length - 1 ? prevState.cursor + 1 : prevState.cursor]
+            })
+            )
+        };
+
+        //up arrow
+        if (e.keyCode === 38) {
+            setDropListState(prevState => ({
+                cursor: prevState.cursor > -1 ? prevState.cursor - 1 : prevState.cursor,
+                value: prevState.cursor > 0 ? dropText[prevState.cursor - 1] : searchTerm
+            })
+            )
+        };
+
+        // enter key
+        if (e.keyCode === 13) {
+            if (dropListState.cursor !== -1) {
+                const field = dropText[dropListState.cursor][1];
+                const value = dropText[dropListState.cursor][0];
+                resetStates();
+                history.push(`/products/search?field=${field}&value=${value}`);
+            }
+        };
+
+        // backspace
+        if (e.keyCode === 8) {
+            if (dropListState.cursor !== -1) {
+                e.preventDefault();
+                setSearchState(searchTerm);
+                setDropListState({ cursor: -1, value: '' })
+            };
+        };
+    };
+
+
+    const handleBlur = () => {
+        window.onclick = e => {
+            // checks if element clicked that is causing the blur 
+            // is not an element from the drop down suggestions list,
+            // otherwise clicking on suggestion doesn't re-route
+            if (!e.target.classList.contains('DropElement')) {
+                resetStates();
+            }
+        };
+    };
+
+
+    // When navigating dropdown list with arrow keys, sets 
+    // search input to current highlighted item in dropdown
+    useEffect(() => {
+        dropListState.cursor !== -1 && setSearchState(dropText[dropListState.cursor][0]);
+        dropListState.cursor === -1 && setSearchState(searchTerm);
+    }, [dropListState, dropText, searchTerm]);
+
+
+    // Retrieve keyword suggestions for dropdown
+    useEffect(() => {
+        const getKeywords = async () => {
+            try {
+                const res = await userReq.get('/products/keywords');
+                var ids = [];
+                var colors = [];
+                var sizes = [];
+                var titles = [];
+                var categories = [];
+                res.data.map((item) => {
+                    ids.push([item._id, 'id']);
+                    titles.push([item.title, 'title']);
+                    item.color.map(color => colors.push(color));
+                    item.size.map(size => sizes.push(size));
+                    item.category.map(category => categories.push(category));
+                    return null
+                });
+                // Arrays of unique values
+                sizes = [...new Set(sizes)].map(size => [size, 'size']);
+                colors = [...new Set(colors)].map(color => [color, 'color']);
+                // Set second value as 'searchcategory' as 'category' already present in backend
+                // for other functionality
+                categories = [...new Set(categories)].map(category => [category, 'searchcategory']);
+                const tempState = {
+                    // ids for later use in admin
+                    ids: ids,
+                    titles: titles,
+                    sizes: sizes,
+                    colors: colors,
+                    categories: categories,
+                    keyWords: titles.concat(colors).concat(sizes).concat(categories)
+                };
+                setKeyWords(tempState);
+            }
+            catch (err) {
+                console.log(err);
+            }
+        };
+        getKeywords();
+    }, [searchState]);
+
+    // Sets dropdown text suggestions according to searchState in the input field
+    useEffect(() => {
+        if (keyWords !== '') {
+            const words = searchState !== '' ? keyWords.keyWords.filter(word => word[0].includes(searchState) || word[0].includes(searchState.toLowerCase()) || word[0].toLowerCase().includes(searchState)) : [];
+            if (words && dropListState.cursor === -1) {
+                setDropText(words);
+                setSearchTerm(searchState);
+            }
+        }
+    }, [searchState, dropListState.cursor, keyWords]);
+
+    return (
+        <Container >
+            <Search >
+                <Input
+                    type="text"
+                    value={searchState}
+                    onChange={handleChange}
+                    onKeyDown={handleKeyDown}
+                    onBlur={handleBlur}
+                />
+                <SearchIcon><i className="fas fa-search"></i></SearchIcon>
+                <ul>
+                    <DropDown>
+                        {dropText && dropText.map((word, index) => {
+                            const splitStart = word[0].toLowerCase().indexOf(searchTerm.toLowerCase());
+                            const splitEnd = splitStart + searchTerm.length;
+                            const highlight = word[0].substring(splitStart, splitEnd);
+                            const before = word[0].substring(0, splitStart);
+                            const after = word[0].substring(splitEnd);
+                            return <DropElement key={index} className='DropElement' style={{ backgroundColor: dropListState.cursor === index ? 'pink' : 'white' }} onClick={() => handleClick(word)}>{before}<strong style={{ color: 'red' }}>{highlight}</strong>{after}</DropElement>
+                        }
+                        )}
+                    </DropDown>
+                </ul>
+            </Search>
+        </Container>
+    )
+}
+
+export default SearchBar

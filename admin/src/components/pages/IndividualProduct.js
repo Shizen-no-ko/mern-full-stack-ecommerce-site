49d98658c8bdeed95308cfa90ba014a0d8e2b191
Mike Search <search_michael@yahoo.com>
2022-03-17 06:57:33 +0100

    Removes axios workaround now handled by interceptor. Tidies-up, comments and formats code.

--- admin/src/components/pages/IndividualProduct.js
+++ admin/src/components/pages/IndividualProduct.js
@@ -1,18 +1,12 @@
 import { useState, useEffect } from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
-import { publicReq, userReq } from '../../axiosRequests';
-import { useDispatch, useSelector } from 'react-redux';
-
 import styled from 'styled-components';
+
+import { publicReq, userReq } from '../../axiosRequests';
 import { mobile, portraitTablet, landscapeTablet } from '../../responsive';
 
 import Navbar from '../layout/Navbar';
-import SubscriptionForm from '../layout/SubscriptionForm';
 import Footer from '../layout/Footer';
-
-// import { sliderData } from '../../data/data.js';
-// import { addProduct } from '../../redux/shoppingCartRedux';
-
 
 const Container = styled.div`
 display:flex;
@@ -22,33 +16,18 @@ max-width: 100%;
 overflow: hidden;
 width: 100vw;
 
-${'' /* ${mobile({
-    height: 'auto'
-})};
-
-
-${portraitTablet({
-    height: 'auto'
-})}; */}
-
 ${landscapeTablet({
     height: 'auto',
     overflow: 'visible'
 })};  
 `
- 
 
 const Wrapper = styled.div`
-${'' /* align-items: center; */}
 display:flex;
-${'' /* transform: translate(${props => props.scrollPos * -100}vw); */}
-${'' /* transition: all 1.5s ease; */}
 width: 100vw;
-
-
 `
+
 const Slide = styled.div`
-${'' /* align-items: center; */}
 display: flex;
 height: auto;
 justify-content: flex-start;
@@ -69,7 +48,7 @@ ${landscapeTablet({
     height: 'auto',
 })};
 `
- 
+
 const DetailsContainer = styled.div`
 flex: 1;
 padding: 20px 80px 20px;
@@ -85,8 +64,6 @@ ${portraitTablet({
 ${landscapeTablet({
     padding: '0px 20px 15px',
 })};
-
-
 `
 
 const Details = styled.div`
@@ -110,73 +87,17 @@ ${landscapeTablet({
     justifyContent: 'space-between'
 })};
 `
-// const Title = styled.h1`
-// font-weight: 400;
-// margin-top: 0;
 
-// ${mobile({
-//     fontSize: '2.5rem',
-//     marginBottom: '0px'
-// })};
-
-// ${portraitTablet({
-//     fontSize: '3rem',
-//     margin: '20px auto 10px'
-// })};
-
-// ${landscapeTablet({
-//     fontSize: '2rem',
-//     margin: '10px 0 10px'
-// })};
-// `
-
-// const Description = styled.p`
-// font-weight: 200;
-
-// ${mobile({
-//     fontSize: '1rem'
-// })};
-
-// ${portraitTablet({
-//     fontSize: '1.25rem'
-// })};
-
-// ${landscapeTablet({
-//     marginTop: '0'
-// })};
-// `
-
-// const Price = styled.h4`
-// font-weight: 200;
-// font-size: 2rem;
-
-// ${mobile({
-//     fontSize: '1.5rem',
-//     margin: '0px'
-// })};
-
-// ${portraitTablet({
-//     fontSize: '1.75rem',
-//     margin: '0px'
-// })};
-
-// ${landscapeTablet({
-//     fontSize: '1.5rem',
-//     margin: '0px'
-// })};
-// `
 const Button = styled.button`
 all: unset;
 background-color: white;
 border: 4px solid red;
 border-radius: 20px 0;
-${'' /* border-radius: 10px; */}
 color: red;
 cursor: pointer;
 font-size: 17px;
 outline: none;
 padding: 10px;
-
 
 &:hover{
     background-color: red;
@@ -203,9 +124,6 @@ ${landscapeTablet({
     fontSize: '18px',
     padding: '5px 10px'
 })};
-
-
-
 `
 
 const ImageContainer = styled.div`
@@ -242,7 +160,6 @@ ${portraitTablet({
 ${landscapeTablet({
     maxHeight: '100%'
 })};
-
 `
 
 const SelectorRow = styled.div`
@@ -267,31 +184,7 @@ ${landscapeTablet({
     padding: '0px',
     flexWrap: 'wrap',
 })};
-
-
-
 `
-
-
-// const SelectorContainer = styled.div`
-// line-height: 30px;
-
-// ${mobile({
-//     display: 'flex',
-//     flexDirection: 'column',
-//     alignItems: 'center',
-// })};
-
-// ${portraitTablet({
-//     display: 'flex',
-//     flexDirection: 'column',
-//     alignItems: 'center',
-// })};
-// `
-
-// const SelectorGroup = styled.div`
-
-// `
 
 const Label = styled.span`
 font-size: 20px;
@@ -311,7 +204,6 @@ ${landscapeTablet({
     paddingLeft: '0'
 })};
 `
-
 
 const ErrorMessage = styled.span`
 background-color: red;
@@ -338,13 +230,13 @@ padding: 10px;
 width: 94%;
 
 ${mobile({
-   fontSize: '15px',
-   marginLeft: '0px'
+    fontSize: '15px',
+    marginLeft: '0px'
 })};
 
 ${portraitTablet({
-   fontSize: '20px',
-   marginLeft: '0px'
+    fontSize: '20px',
+    marginLeft: '0px'
 })};
 `
 
@@ -370,26 +262,27 @@ ${portraitTablet({
 
 
 const IndividualProduct = (props) => {
+
     const history = useHistory();
-    // console.log(history);
+
+
     const path = useLocation().pathname.split('/');
-    const id = path[path.length -1];
+    const id = path[path.length - 1];
+
     const { add } = props;
-  
-   
+
     const [formData, setFormData] = useState({
         _id: '',
         title: '',
-        description:'',
+        description: '',
         image: '',
         category: [],
         size: [],
         color: [],
         price: '',
-        inStock: false 
+        inStock: false
     });
 
-    // const dispatch = useDispatch();
 
     const { _id, title, description, image, category, size, color, price, inStock } = formData;
 
@@ -397,74 +290,51 @@ const IndividualProduct = (props) => {
 
 
     useEffect(() => {
-        let isMounted = true;
         try {
             const getProduct = async () => {
                 const res = await publicReq.get(`products/find/${id}`);
-                // setDisplayProduct(res.data);
-                // setSelectedColor(res.data.color[0]);
-                if(res){  
+                if (res) {
                     setFormData(res.data);
                     setErrorMessage('');
                 } else {
                     setErrorMessage('No Document with this ID');
                 }
-              
-                
             }
             // only retrieve product by id if module not being used to add product
-            if(!add){getProduct()}
-            
+            if (!add) { getProduct() }
         }
-        catch (err) { console.log(err) }
-        return () => { isMounted = false };
+        catch (err) { setErrorMessage(err.response.data.errors[0].msg) }
     }, [id])
 
     const handleUpdate = async () => {
-        // set token for headers here as in axiosRequests it seems to be set at startup and doesn't update
-        const CURRENT_USER = localStorage.length > 0 ? JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser : null;
-        const TOKEN =  CURRENT_USER ? CURRENT_USER.token : null;
-            try {
-                const headers = {
-                    token: localStorage.length > 0 ? `Bearer ${TOKEN}` : null
-                 }
-                 // if these fields have been altered - set to lowercase, split and trim, ready for putting to DB, otherwise leave as is
-                 const tidyData = {...formData, 
-                    color: typeof color === 'string' ? color.toLowerCase().split(',').map(item => item.trim()): color,
-                    size: typeof size === 'string' ? size.toLowerCase().split(',').map(item => item.trim()): size,
-                    category: typeof category === 'string' ? category.toLowerCase().split(',').map(item => item.trim()): category
-                }
-                const res = await userReq.put(`products/${id}`, tidyData, { headers: headers} );
-                // update form - force re-render with tidied updates
-                setFormData(tidyData);
-                console.log(`response is ${res.data}`);
+        try {
+            // if these fields have been altered - set to lowercase, split and trim, ready for putting to DB, otherwise leave as is
+            const tidyData = {
+                ...formData,
+                color: typeof color === 'string' ? color.toLowerCase().split(',').map(item => item.trim()) : color,
+                size: typeof size === 'string' ? size.toLowerCase().split(',').map(item => item.trim()) : size,
+                category: typeof category === 'string' ? category.toLowerCase().split(',').map(item => item.trim()) : category
+            }
+            const res = await userReq.put(`products/${id}`, tidyData);
+            // update form - force re-render with tidied updates
+            setFormData(tidyData);
         }
-        catch (err) { 
+        catch (err) {
             setErrorMessage(err.response.data.errors[0].msg)
-            // console.log(err.response.data.errors[0].msg) 
-        }  
+        }
     };
 
+
     const handleDelete = async () => {
-         // set token for headers here as in axiosRequests it seems to be set at startup and doesn't update
-         const CURRENT_USER = localStorage.length > 0 ? JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser : null;
-         const TOKEN =  CURRENT_USER ? CURRENT_USER.token : null;
-         try {
-            const headers = {
-                token: localStorage.length > 0 ? `Bearer ${TOKEN}` : null
-             }
-            const res = await userReq.delete(`products/${id}`,{ headers: headers} );
-            console.log(`response is ${res.data}`);
+        try {
+            await userReq.delete(`products/${id}`);
             setErrorMessage('Product has been moved to deleted products collection');
             // replace history so that user can't back arrow to products page with deleted id
             history.replace('/');
-            
-    }
-    catch (err) { 
-        setErrorMessage(err.response.data.errors[0].msg)
-        // console.log(err.response.data.errors[0].msg) 
-    }
-       
+        }
+        catch (err) {
+            setErrorMessage(err.response.data.errors[0].msg)
+        }
     };
 
     const handleReset = () => {
@@ -472,7 +342,7 @@ const IndividualProduct = (props) => {
             {
                 _id: '',
                 title: '',
-                description:'',
+                description: '',
                 image: '',
                 category: [],
                 size: [],
@@ -484,97 +354,82 @@ const IndividualProduct = (props) => {
     };
 
     const handleAdd = async () => {
-        // set token for headers here as in axiosRequests it seems to be set at startup and doesn't update
-        const CURRENT_USER = localStorage.length > 0 ? JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser : null;
-        const TOKEN =  CURRENT_USER ? CURRENT_USER.token : null;
-            try {
-                const headers = {
-                    token: localStorage.length > 0 ? `Bearer ${TOKEN}` : null
-                 };
-                 // if these fields have been altered - set to lowercase, split and trim, ready for putting to DB, otherwise leave as is
-                 const tidyData = {...formData, 
-                    color: typeof color === 'string' ? color.toLowerCase().split(',').map(item => item.trim()): color,
-                    size: typeof size === 'string' ? size.toLowerCase().split(',').map(item => item.trim()): size,
-                    category: typeof category === 'string' ? category.toLowerCase().split(',').map(item => item.trim()): category
-                };
-                // remove id key before posting
-                delete tidyData._id; 
-                const res = await userReq.post('products/add', tidyData, { headers: headers} );
-                // update form - force re-render with tidied updates
-                setFormData(tidyData);
-                history.replace(`/product/${res.data._id}`);
+        try {
+            // if these fields have been altered - set to lowercase, split and trim, ready for putting to DB, otherwise leave as is
+            const tidyData = {
+                ...formData,
+                color: typeof color === 'string' ? color.toLowerCase().split(',').map(item => item.trim()) : color,
+                size: typeof size === 'string' ? size.toLowerCase().split(',').map(item => item.trim()) : size,
+                category: typeof category === 'string' ? category.toLowerCase().split(',').map(item => item.trim()) : category
+            };
+            // remove id key before posting
+            delete tidyData._id;
+            const res = await userReq.post('products/add', tidyData);
+            // update form - force re-render with tidied updates
+            setFormData(tidyData);
+            history.replace(`/product/${res.data._id}`);
         }
-        catch (err) { 
+        catch (err) {
             setErrorMessage(err.response.data.errors[0].msg)
-            // console.log(err.response.data.errors[0].msg) 
-        }  
+        }
     };
 
-    const onChange = (e) => setFormData({...formData, [e.target.name]: e.target.value});
-    const setStock = (value) => setFormData({...formData, inStock: value });
+    const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+    const setStock = (value) => setFormData({ ...formData, inStock: value });
 
-const onSubmit = (e) => {
-    e.preventDefault();
-    
-};
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
 
     return (
         <div>
             <Navbar />
             <Container>
-                {/* <Wrapper scrollPos={scrollPos} key={i}> */}
                 <Wrapper>
                     <Slide >
                         <ImageContainer>
-                           {add ? null : <Img src={image} />} 
+                            {add ? null : <Img src={image} />}
                         </ImageContainer>
                         <DetailsContainer>
                             <Details>
-                           
-                            <Title>{add ? 'Add Product' : 'Edit Product'}</Title>
-                            <Form onSubmit={e => onSubmit(e)}>
-                            <Label>Title</Label>
-                    <Input onChange={e => onChange(e)} required name='title' type='text' value={title}></Input>
-                   {add? null : <Label>Product Id: <strong>{_id}</strong></Label> } 
-                    <Label>Image Url</Label>
-                        <Input onChange={e => onChange(e)} required name='image' type='text' value={image}></Input>
-                        <Label>Price</Label>
-                        <Input onChange={e => onChange(e)} required name='price' type='number' value={price} ></Input>
-                        <Label>Description</Label>
-                        <Input onChange={e => onChange(e)} required name='description' type='text' value={description} ></Input>
-                        <Label>Categories (Separated by commas)</Label>
-                        <Input onChange={e => onChange(e)} required name='category' type='text' value={category} ></Input>
-                        <Label>Colors (Separated by commas)</Label>
-                        <Input onChange={e => onChange(e)}  name='color' type='text' value={color} ></Input>
-                        <Label>Sizes (Separated by commas)</Label>
-                        <Input onChange={e => onChange(e)}  name='size' type='text' value={size} ></Input>
-                        <Label>In Stock                         <CheckBox type='checkBox' label="In Stock" checked={inStock} value={inStock} onChange={() => setStock(!inStock)}  /></Label>
-                        {/* onClick={() => setStock(!inStock)} */}
-                    </Form>
-                   {errorMessage !== '' ? <ErrorMessage>{errorMessage}</ErrorMessage>: null} 
-                   {add ? 
-                    <SelectorRow>
-                                    <Button onClick={handleReset}><i className="fas fa-eraser"></i> RESET PRODUCT</Button>
-                                    <Button onClick={handleAdd}><i className="far fa-save"></i> ADD PRODUCT</Button>
-                                </SelectorRow>
-                   :
-                   <SelectorRow>
-                                    <Button onClick={handleUpdate}><i className="fas fa-edit"></i> UPDATE PRODUCT</Button>
-                                    <Button onClick={handleDelete}><i className="fas fa-trash-alt"></i> DELETE PRODUCT</Button>
-                                </SelectorRow>
-                    }
-                                
-                             
+                                <Title>{add ? 'Add Product' : 'Edit Product'}</Title>
+                                <Form onSubmit={e => onSubmit(e)}>
+                                    <Label>Title</Label>
+                                    <Input onChange={e => onChange(e)} required name='title' type='text' value={title}></Input>
+                                    {add ? null : <Label>Product Id: <strong>{_id}</strong></Label>}
+                                    <Label>Image Url</Label>
+                                    <Input onChange={e => onChange(e)} required name='image' type='text' value={image}></Input>
+                                    <Label>Price</Label>
+                                    <Input onChange={e => onChange(e)} required name='price' type='number' value={price} ></Input>
+                                    <Label>Description</Label>
+                                    <Input onChange={e => onChange(e)} required name='description' type='text' value={description} ></Input>
+                                    <Label>Categories (Separated by commas)</Label>
+                                    <Input onChange={e => onChange(e)} required name='category' type='text' value={category} ></Input>
+                                    <Label>Colors (Separated by commas)</Label>
+                                    <Input onChange={e => onChange(e)} name='color' type='text' value={color} ></Input>
+                                    <Label>Sizes (Separated by commas)</Label>
+                                    <Input onChange={e => onChange(e)} name='size' type='text' value={size} ></Input>
+                                    <Label>In Stock                         <CheckBox type='checkBox' label="In Stock" checked={inStock} value={inStock} onChange={() => setStock(!inStock)} /></Label>
+                                </Form>
+                                {errorMessage !== '' ? <ErrorMessage>{errorMessage}</ErrorMessage> : null}
+                                {add ?
+                                    <SelectorRow>
+                                        <Button onClick={handleReset}><i className="fas fa-eraser"></i> RESET PRODUCT</Button>
+                                        <Button onClick={handleAdd}><i className="far fa-save"></i> ADD PRODUCT</Button>
+                                    </SelectorRow>
+                                    :
+                                    <SelectorRow>
+                                        <Button onClick={handleUpdate}><i className="fas fa-edit"></i> UPDATE PRODUCT</Button>
+                                        <Button onClick={handleDelete}><i className="fas fa-trash-alt"></i> DELETE PRODUCT</Button>
+                                    </SelectorRow>
+                                }
                             </Details>
                         </DetailsContainer>
                     </Slide>
                 </Wrapper>
             </Container>
-
-            {/* <SubscriptionForm /> */}
             <Footer />
         </div>
-
     )
 }
 

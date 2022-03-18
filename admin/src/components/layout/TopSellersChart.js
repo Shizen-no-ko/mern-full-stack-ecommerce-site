18ef2f201722bc3267fd331aeb7f50c34f178f7c
Mike Search <search_michael@yahoo.com>
2022-03-18 09:34:18 +0100

    Tidies-up, comments and formats code

--- admin/src/components/layout/TopSellersChart.js
+++ admin/src/components/layout/TopSellersChart.js
@@ -5,7 +5,6 @@ import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
 import { userReq } from '../../axiosRequests';
 
 
-
 const Container = styled.div`
 align-items: center;
 border: 1px solid lightgray;
@@ -26,62 +25,56 @@ text-align: left;
 width: 100%;
 `
 
-
-
 const Title = styled.h3`
 margin: 5px 10px;
 padding: 0px;
 `
 
-
-
 const TopSellersChart = () => {
 
-    const [topData, setTopData ] = useState([]);
-    const [barChart, setBarChart] = useState(<BarChart/>);
-    
+    const [topData, setTopData] = useState([]);
+    const [barChart, setBarChart] = useState(<BarChart />);
 
+    // Retrieve top selling products from api
     useEffect(() => {
-
         const getTopData = async () => {
             try {
                 const res = await userReq.get('/orders/topsellers');
-                if(res){
+                if (res) {
                     setTopData(res.data);
-                } 
+                }
             }
             catch (err) { console.log(err) };
-
         };
         getTopData();
     }, []);
 
+
+
     useEffect(() => {
-       
-            const getNames = async () => {
-                try{
+        const getNames = async () => {
+            // Retrieve ids from items
+            try {
                 const ids = topData.map((item) => {
                     return item._id;
                 })
-                console.log("IDs are");
-                console.log(ids);
+                // Find all products with ids corresponding to ids in topData
                 const res = await userReq.get(`/products/findall/${ids}`);
                 const tempData = topData;
-                res.data.map((item ) => {
-                    return tempData.find(x => x._id === item._id ).title = item.title.substring(0, 8);
+                // Map through and append title to items in tempData
+                res.data.map((item) => {
+                    return tempData.find(x => x._id === item._id).title = item.title.substring(0, 8);
                 })
                 setTopData(tempData);
                 setBarChart(
                     <BarChart width={500} height={200} data={topData} margin={{ top: 5, right: 20, bottom: 20, left: 0 }}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis style={{fontSize: '0.8rem'}} dataKey="title" label={{ value: 'Products', offset: '-10', position: 'insideBottom' }}  />
-              <YAxis dataKey="count" label={{ value: 'Units Sold', angle: -90, position: 'insideBottomLeft', offset: '15' }} />
-              <Tooltip />
-              {/* <Legend /> */}
-              <Bar barSize={25} dataKey="count" fill="#8884d8" />
-            </BarChart>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis style={{ fontSize: '0.8rem' }} dataKey="title" label={{ value: 'Products', offset: '-10', position: 'insideBottom' }} />
+                        <YAxis dataKey="count" label={{ value: 'Units Sold', angle: -90, position: 'insideBottomLeft', offset: '15' }} />
+                        <Tooltip />
+                        <Bar barSize={25} dataKey="count" fill="#8884d8" />
+                    </BarChart>
                 )
-                
             }
             catch (err) {
                 console.log(err)
@@ -94,12 +87,11 @@ const TopSellersChart = () => {
     return (
         <div>
             <Container>
-            <TitleDiv>
-            <Title>Top Selling Products</Title>
-            </TitleDiv>
-            {barChart}
+                <TitleDiv>
+                    <Title>Top Selling Products</Title>
+                </TitleDiv>
+                {barChart}
             </Container>
-
         </div>
     )
 }

0a8157954db8238a127ce32cd2498fe24cce2c9d
Mike Search <search_michael@yahoo.com>
2022-03-18 08:28:48 +0100

    Fixes logic error causing infinite API calls

--- admin/src/components/layout/TopEarnersChart.js
+++ admin/src/components/layout/TopEarnersChart.js
@@ -34,19 +34,94 @@ padding: 0px;
 
 const TopEarnersChart = () => {
 
+    const [ids, setIds] = useState([]);
+
     const [topData, setTopData ] = useState([]);
+    const [chartData, setChartData] = useState();
     const [barChart, setBarChart] = useState(<BarChart/>);
     
+    // // Get all unique sales items with amount sold per item
+    // useEffect(() => {
+    //     const getTopData = async () => {
+    //         console.log('Top Earners Chart get top data');
+    //         try {
+    //             const res = await userReq.get('/orders/allsales');
+    //             if(res){
+    //                 console.log('TOP EARNERS DATA');
+    //                 console.log(res.data);
+    //                 setTopData(res.data);
+    //             } 
+    //         }
+    //         catch (err) { console.log(err) };
 
-    useEffect(() => {
+    //     };
+    //     getTopData();
+    // }, []);
 
+    // useEffect(() => {
+    //    console.log('Top Earners chart get Names create bar chart');
+    //         const getNames = async () => {
+    //             try{
+    //             const ids = topData.map((item) => {
+    //                 return item._id;
+    //             })
+    //             console.log("IDs are");
+    //             console.log(ids);
+    //             const res = await userReq.get(`/products/findall/${ids}`);
+    //             const tempData = topData;
+    //             res.data.map((item) => {
+    //                 tempData.find(x => x._id === item._id ).price = item.price;
+    //                return tempData.find(x => x._id === item._id ).title = item.title.substring(0, 8);   
+    //             })
+    //             tempData.map(item => {
+    //                return item.totalEarned = item.price * item.count;
+    //             })
+    //             tempData.sort((a, b) => b.totalEarned - a.totalEarned);
+    //             if(chartData === []){
+    //                 setChartData(tempData.slice(0,5));
+    //             }
+                
+    //             console.log('Chart data is ' + chartData);
+    //             setBarChart(
+    //                 <BarChart width={500} height={200} data={chartData} margin={{ top: 5, right: 20, bottom: 20, left: 0 }}>
+    //           <CartesianGrid strokeDasharray="3 3" />
+    //           <XAxis style={{fontSize: '0.8rem'}} dataKey="title" label={{ value: 'Products', offset: '-10', position: 'insideBottom' }}  />
+    //           <YAxis dataKey="totalEarned" label={{ value: 'Total Earned USD', angle: -90, position: 'insideBottomLeft', offset: '10' }} />
+    //           <Tooltip />
+    //           <Bar barSize={25} dataKey="totalEarned" fill="#8884d8" />
+    //         </BarChart>
+    //             )
+                
+    //         }
+    //         catch (err) {
+    //             console.log(err)
+    //         }
+    //     }
+    //     getNames();
+    // }, [topData, chartData])
+
+    
+ 
+    // REBUILD!!!!
+
+     // Get all unique sales items with amount sold per item
+     useEffect(() => {
         const getTopData = async () => {
+            console.log('Top Earners Chart get top data');
             try {
+                // Get all sales items via API; with amount sold for each item
                 const res = await userReq.get('/orders/allsales');
                 if(res){
                     console.log('TOP EARNERS DATA');
                     console.log(res.data);
                     setTopData(res.data);
+                    // Get ids of each item to search for products' details
+                    const resIds = res.data.map((item) => {
+                        return item._id;
+                    })
+                    console.log("IDs are");
+                console.log(resIds);
+                    setIds(resIds);
                 } 
             }
             catch (err) { console.log(err) };
@@ -56,33 +131,36 @@ const TopEarnersChart = () => {
     }, []);
 
     useEffect(() => {
-       
+       console.log('Top Earners chart get Names create bar chart');
             const getNames = async () => {
                 try{
-                const ids = topData.map((item) => {
-                    return item._id;
-                })
-                console.log("IDs are");
-                console.log(ids);
+                // Retrieve all 
                 const res = await userReq.get(`/products/findall/${ids}`);
-                const tempData = topData;
+                const tempData = [...topData];
                 res.data.map((item) => {
+                    // Find item in tempData with same id as current res item and 
+                    //set the price for corresponding item in tempData
                     tempData.find(x => x._id === item._id ).price = item.price;
+                    // Likewise set a cropped title for corresponding item
                    return tempData.find(x => x._id === item._id ).title = item.title.substring(0, 8);   
                 })
                 tempData.map(item => {
+                    // Calculate and set totalEarned
                    return item.totalEarned = item.price * item.count;
                 })
+                // Sort by descending
                 tempData.sort((a, b) => b.totalEarned - a.totalEarned);
-                setTopData(tempData.slice(0,5));
-                console.log(topData);
+                
+                !chartData && setChartData([...tempData.slice(0,5)]);
+                
+                console.log('Chart data is ');
+                console.log(chartData);
                 setBarChart(
-                    <BarChart width={500} height={200} data={topData} margin={{ top: 5, right: 20, bottom: 20, left: 0 }}>
+                    <BarChart width={500} height={200} data={chartData} margin={{ top: 5, right: 20, bottom: 20, left: 0 }}>
               <CartesianGrid strokeDasharray="3 3" />
               <XAxis style={{fontSize: '0.8rem'}} dataKey="title" label={{ value: 'Products', offset: '-10', position: 'insideBottom' }}  />
               <YAxis dataKey="totalEarned" label={{ value: 'Total Earned USD', angle: -90, position: 'insideBottomLeft', offset: '10' }} />
               <Tooltip />
-              {/* <Legend /> */}
               <Bar barSize={25} dataKey="totalEarned" fill="#8884d8" />
             </BarChart>
                 )
@@ -93,7 +171,7 @@ const TopEarnersChart = () => {
             }
         }
         getNames();
-    }, [topData])
+    }, [topData, chartData, ids])
 
 
     return (
